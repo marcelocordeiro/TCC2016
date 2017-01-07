e3984fff660f8cf1c8de308297b5b409f770ed98
Marcelo Cordeiro <marcelo.augusto.cordeiro@gmail.com>
2017-01-07 16:52:36 -0200

    App.js basico

---
+++ Web/node/app.js
@@ -0,0 +1,26 @@
+/**
+ * app.js - Main Node.js Express Application Controller
+ */
+
+// Module dependencies.
+var express = require(‘express'),
+    pug = require('pug'),
+    path = require('path'),
+    home = require('./routes/home');
+
+var app = express();
+
+// Setup the application's environment.
+app.set('port',  process.env._EJS_APP_PORT || 3000);
+app.set('host',  process.env.EJS_APP_HOST || 'localhost');
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route all GET requests to our public static index.html page
+app.get('/', home.index);
+
+// Start listening for requests
+var server = app.listen(app.get('port'), app.get('host'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
